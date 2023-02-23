d990c0816735b80334482d821628051719ce29e1
Luciano Petrate <lucianopetrate@gmail.com>
2023-02-23 18:54:57 -0300

    database connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+async function getLinks() {
+  const res = await fetch(
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQmCGsxyOLbB63Qg9SaPMEbs0C1dk8PLE1WmTeMjHOYNhIe2VnU6XhAqXTPBFwU1uo7bwiZL1yxfXaA/pub?gid=0&single=true&output=csv"
+  );
+  const data = await res.text();
+  return data;
+}
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      que onda perro?
-    </main>
-  )
+export default async function Home() {
+  const links = await getLinks()
+
+  console.log(links);
+  return <main className={styles.main}>que onda perro?</main>;
 }
